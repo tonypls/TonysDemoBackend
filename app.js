af57191bfa365ee945c4d54b5ec3b09a1365191c
Tony <toe_knee_van@hotmail.com>
2018-06-24 17:24:43 +1200

    Displaying json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ A simple Language Understanding (LUIS) bot for the Microsoft Bot Framework.
 var restify = require('restify');
 var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
-var tools = require('./tools.js');
+//var tools = require('./tools.js');
 var path = require('path');
 var names = require("./db.json");
 
