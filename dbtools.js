5e86fc407be705641eb80fc5a5536cdd1e54b34c
Tony <toe_knee_van@hotmail.com>
2018-06-24 19:21:29 +1200

    Displaying json

--- dbtools.js
+++ dbtools.js
@@ -2,9 +2,9 @@ var peopleData = require("./db.json");
 
 module.exports = {
   getNames : function(){
-    var people;
+    var people ="";
     for (var i = 1; i < peopleData.people.length; i++){
-      people += peopleData.people[i].name + " is " + peopleData.people[i].age + " years old and" +  peopleData.people[i].gender + ", ";
+      people += peopleData.people[i].name + " is " + peopleData.people[i].age + " years old and " +  peopleData.people[i].gender + ", ";
       }
     return people;
   }
