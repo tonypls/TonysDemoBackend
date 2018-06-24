647a2a23da185e51fe7cc4f927d647183a71c885
Tony <toe_knee_van@hotmail.com>
2018-06-24 19:26:48 +1200

    Displaying json

--- dbtools.js
+++ dbtools.js
@@ -3,37 +3,43 @@ var peopleData = require("./db.json");
 module.exports = {
   getNames : function(){
     var people ="";
-    for (var i = 1; i < peopleData.people.length; i++){
+    for (var i = 0; i < peopleData.people.length; i++){
       people += peopleData.people[i].name + " is " + peopleData.people[i].age + " years old and " +  peopleData.people[i].gender + ", ";
       }
     return people;
+  },
+  getFemales : function(){
+    var people= "";
+    for (var i = 0; i < peopleData.people.length; i++){
+      if(peopleData.people[i].gender === "female"){
+        people += peopleData.people[i].name;
+      }
+    }
+    return people;
+  },
+  getMales : function(){
+    var people= "";
+    for (var i = 0; i < peopleData.people.length; i++){
+      if(peopleData.people[i].gender === "male"){
+        people += peopleData.people[i].name;
+      }
+    }
+    return people;
+  },
+  getOverAge : function(age){
+    var people= "";
+    for (var i = 0; i < peopleData.people.length; i++){
+      if(peopleData.people[i].age > age)
+      people += peopleData.people[i].name;
+    }
+    return people;
+  },
+  getUnderAge : function(age){
+    var people= "";
+    for (var i = 0; i < peopleData.people.length; i++){
+      if(peopleData.people[i].age < age)
+      people += peopleData.people[i].name;
+    }
+    return people;
   }
-  // getFemales : function(){
-  //   var people;
-  //   for (var i = 0; i < peopleData.people.length; i++){
-  //     people += peopleData.people[i].name + " is " + peopleData.people[i].age;
-  //   }
-  //   return people;
-  // },
-  // getMales : function(){
-  //   var people;
-  //   for (var i = 0; i < peopleData.people.length; i++){
-  //     people += peopleData.people[i].name + " is " + peopleData.people[i].age;
-  //   }
-  //   return people;
-  // },
-  // getOverAge : function(age){
-  //   var people;
-  //   for (var i = 0; i < peopleData.people.length; i++){
-  //     people += peopleData.people[i].name + " is " + peopleData.people[i].age;
-  //   }
-  //   return people;
-  // },
-  // getUnderAge : function(age){
-  //   var people;
-  //   for (var i = 0; i < peopleData.people.length; i++){
-  //     people += peopleData.people[i].name + " is " + peopleData.people[i].age;
-  //   }
-  //   return people;
-  // }
 };
