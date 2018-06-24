b946ad4a01fee966f9c44c95b34cb759040cd36a
Tony <toe_knee_van@hotmail.com>
2018-06-24 19:05:37 +1200

    Displaying json

--- dbtools.js
+++ dbtools.js
@@ -5,7 +5,36 @@ module.exports = {
     var people;
     for (var i = 0; i < names.length; i++){
       people += names[i].name + " is " + names[i].age;
+      console.log(people);
     }
     return people;
   }
+  // getFemales : function(){
+  //   var people;
+  //   for (var i = 0; i < names.length; i++){
+  //     people += names[i].name + " is " + names[i].age;
+  //   }
+  //   return people;
+  // },
+  // getMales : function(){
+  //   var people;
+  //   for (var i = 0; i < names.length; i++){
+  //     people += names[i].name + " is " + names[i].age;
+  //   }
+  //   return people;
+  // },
+  // getOverAge : function(age){
+  //   var people;
+  //   for (var i = 0; i < names.length; i++){
+  //     people += names[i].name + " is " + names[i].age;
+  //   }
+  //   return people;
+  // },
+  // getUnderAge : function(age){
+  //   var people;
+  //   for (var i = 0; i < names.length; i++){
+  //     people += names[i].name + " is " + names[i].age;
+  //   }
+  //   return people;
+  // }
 };
